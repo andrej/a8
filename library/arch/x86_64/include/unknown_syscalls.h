a6566b751105dc5b3d342bec59857454a4f20c83
andrej <an.roesti@gmail.com>
2023-03-13 10:41:18 -0700

    fix x86 codebase with new unknown system calls

--- library/arch/x86_64/include/unknown_syscalls.h
+++ library/arch/x86_64/include/unknown_syscalls.h
@@ -1,4 +1,12 @@
 #ifndef UNKNOWN_SYSCALLS_H
 #define UNKNOWN_SYSCALLS_H
 
+#define UNKNOWN_SYSCALL (MAX_SYSCALL_NO+1)
+
+#define __NR_wait   UNKNOWN_SYSCALL
+#define __NR_wait3  UNKNOWN_SYSCALL
+#define __NR_waitpid UNKNOWN_SYSCALL
+#define __NR_pread  UNKNOWN_SYSCALL
+#define __NR_pwrite UNKNOWN_SYSCALL
+
 #endif

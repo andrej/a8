9f32647d1a8eb0dbfde648bd27c6d4501ec8d068
André Rösti <an.roesti@gmail.com>
2024-08-28 10:47:37 +0000

    small cleanups

--- scripts/stop_monmod.sh
+++ scripts/stop_monmod.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+script_dir=$(realpath $(dirname $0))
+
 rm -f /dev/shm/vmas_smem_*
 killall -9 monmod_run.sh lighttpd nginx redis-server vma-server
+
+sudo -S ${script_dir}/reset_monmod.sh 
